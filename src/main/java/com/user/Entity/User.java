78e8eea448370a89867392ddec4701e8379044bf
shaan <shaanyworkspace@gmail.com>
2025-12-21 13:23:44 +0530

    updated

--- src/main/java/com/user/Entity/User.java
+++ src/main/java/com/user/Entity/User.java
@@ -28,7 +28,6 @@ public class User {
 	private String password;
 
 	@ElementCollection(fetch = FetchType.EAGER)
-	@CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
 	@Enumerated(EnumType.STRING)
 	private List<Role> roles;
 }
