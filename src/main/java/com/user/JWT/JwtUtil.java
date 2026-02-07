754d0282dcaadcbf258b2f37a775c8386c055c1c
shaan <shaanyworkspace@gmail.com>
2026-02-07 17:33:00 +0530

    Add GitHub Actions workflow for OCI deployment

--- src/main/java/com/user/JWT/JwtUtil.java
+++ src/main/java/com/user/JWT/JwtUtil.java
@@ -15,7 +15,7 @@ import java.util.Date;
 @Component
 public class JwtUtil {
 
-	@Value("${jwt.secretkey}")
+	@Value("${jwtSecretKeyENV}")
 	private String jwtSecretKey;
 
 	private SecretKey getSecretKey(){
